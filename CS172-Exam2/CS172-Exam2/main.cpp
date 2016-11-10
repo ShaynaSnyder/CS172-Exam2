6b289de68d0eb2b82b99cdb85a3a6bd24767cdf4
Shayna Snyder <ssnyder19@my.whitworth.edu>
2016-11-10 14:09:06 -0800

    Initial Commit

---
+++ CS172-Exam2/CS172-Exam2/main.cpp
@@ -0,0 +1,16 @@
+//Shayna Snyder
+//CS-172
+//10 November 2016
+//Exam 2
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
