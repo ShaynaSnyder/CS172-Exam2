08fd2c1ac04c9d3bf8859e13094fc40ab791d20d
Shayna Snyder <ssnyder19@my.whitworth.edu>
2016-11-10 19:46:16 -0800

    Problem setup

---
+++ CS172-Exam2/CS172-Exam2/Citizen.cpp
@@ -0,0 +1,9 @@
+//
+//  Citizen.cpp
+//  CS172-Exam2
+//
+//  Created by Shayna Snyder on 11/10/16.
+//  Copyright © 2016 Shayna Snyder. All rights reserved.
+//
+
+#include "Citizen.hpp"
---
+++ CS172-Exam2/CS172-Exam2/City.cpp
@@ -0,0 +1,9 @@
+//
+//  City.cpp
+//  CS172-Exam2
+//
+//  Created by Shayna Snyder on 11/10/16.
+//  Copyright © 2016 Shayna Snyder. All rights reserved.
+//
+
+#include "City.hpp"
